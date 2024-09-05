591e69059c20a95a8e4678274e2c94a6e5298e0d
qasmark <99826972+qasmark@users.noreply.github.com>
2024-09-05 19:03:55 +0300

    didn't choose and just did two formulas

--- lw1/task5.hs
+++ lw1/task5.hs
@@ -1,10 +1,20 @@
--- 5. Список пирамидальных чисел Ферма. Количество чисел в списке = 50
--- Взял как число точек в четырехугольных пирамидах 
-pyramidalNumbers :: Int -> [Int]
-pyramidalNumbers k = take k [n * (n + 1) * (2 * n + 1) `div` 6 | n <- [1..]]
+-- 5. Список пирамидальных чисел Ферма. Количество чисел в списке = 50 
+-- Функция для вычисления пирамидальных чисел
+pyramidal :: (Int -> Int) -> Int -> [Int]
+pyramidal formula k = take k [formula n | n <- [1..]]
+
+-- Формула для четырехугольной пирамиды
+fourPyramidal :: Int -> Int
+fourPyramidal n = n * (n + 1) * (2 * n + 1) `div` 6
+
+-- Формула для треугольной пирамиды
+triangularPyramidal :: Int -> Int
+triangularPyramidal n =  n * (n + 1) * (n + 2) `div` 6
 
 main :: IO ()
 main = do
     let count = 50
-    print (pyramidalNumbers count)
-    print (length (pyramidalNumbers count))
+    putStrLn "Four-angle pyramid:"
+    print (pyramidal fourPyramidal count)
+    putStrLn "Triangle pyramid:"
+    print (pyramidal triangularPyramidal count)

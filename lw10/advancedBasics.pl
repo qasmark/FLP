571a56ca07a3db6dc804c2eb19ce5e6ecd9dd270
qasmark <99826972+qasmark@users.noreply.github.com>
2024-10-10 12:47:30 +0300

    added passed lab 10
    Чтобы до такого додуматься, мне пришлось рисовать

---
+++ lw10/advancedBasics.pl
@@ -0,0 +1,40 @@
+% Task 1
+seg(1, point(1, 11), point(14, 11)).
+seg(2, point(2, 4), point(13, 4)).
+seg(3, point(2, 2), point(9, 2)).
+seg(4, point(3, 1), point(3, 10)).
+seg(5, point(7, 10), point(13, 10)).
+seg(6, point(8, 0), point(8, 13)).
+seg(7, point(10, 3), point(10, 12)).
+seg(8, point(11, 3), point(11, 13)).
+seg(9, point(12, 2), point(12, 12)).
+
+horiz(N) :- seg(N, point(_, Y), point(_, Y)).
+vertical(N) :- seg(N, point(X, _), point(X, _)).
+
+% Task 2
+cross(N, M, point(X, Y), NL, ML) :-
+    % Я гений 
+    (
+        % N - horizontal, M - vertical
+        horiz(N), vertical(M),
+        seg(N, point(X1, Y), point(X2, Y)),
+        seg(M, point(X, Y1), point(X, Y2)),
+        X1 =< X, X =< X2, Y1 =< Y, Y =< Y2,
+        NL is abs(X2 - X1),
+        ML is abs(Y2 - Y1)
+    ;
+        % M - horizontal, N - vertical
+        vertical(N), horiz(M),
+        seg(N, point(X, Y1), point(X, Y2)),
+        seg(M, point(X1, Y), point(X2, Y)),
+        X1 =< X, X =< X2, Y1 =< Y, Y =< Y2,
+        NL is abs(Y2 - Y1),
+        ML is abs(X2 - X1)
+    ).
+% Task 3
+perimetr(A, B, C, D, P, S) :-
+    cross(A, B, point(X1, Y1), _, _),
+    cross(C, D, point(X2, Y2), _, _),
+    P is 2 * (abs(X2 - X1) + abs(Y2 - Y1)),
+    S is abs(X2 - X1) * abs(Y2 - Y1).

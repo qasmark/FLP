51fd87d76416055d6adb5d11980b8e44977dc47c
qasmark <99826972+qasmark@users.noreply.github.com>
2024-10-28 20:27:58 +0300

    removed comments

--- Haskell/LogicalTransportation/autotests.bat
+++ Haskell/LogicalTransportation/autotests.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 
-:: Compile the Haskell program
 echo Compiling Haskell program...
 ghc -o trans trans.hs
 if errorlevel 1 (
@@ -9,24 +8,17 @@ if errorlevel 1 (
     exit /b 1
 )
 
-:: Iterate over each test file in the tests directory
 for %%f in (tests\*.txt) do (
     echo Running test %%~nf...
     
-    :: Run the compiled Haskell program with the test inputs from the file
     trans.exe < %%f > output.txt
-    
-    :: Display the test result
     type output.txt
     
     echo Test %%~nf completed.
     echo ---------------------------------
-    
-    :: Insert a blank line
     echo.
 )
 
-:: Clean up
 del trans.exe
 del output.txt
 
